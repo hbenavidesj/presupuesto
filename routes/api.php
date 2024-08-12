48248c60fcaab591acda68416f852527bfa333ca
hbenavidesj <h_benavidesj@yahoo.com>
2024-08-11 23:24:38 -0500

    agostosto_actualizacion2
    actualización con endpoint de usuarios

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\personapiController;
+use App\Http\Controllers\Api\userapiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('people',personapiController::class);
+Route::apiResource('users',userapiController::class);
